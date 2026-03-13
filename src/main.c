94d0ffe5968ecbef4654dcdce6828e354eceaec4
chids04 <guyepic39@gmail.com>
2026-03-13 14:59:56 +0000

    passing all cpu test roms but interrupts (handlers not implemented)

--- src/main.c
+++ src/main.c
@@ -7,7 +7,10 @@
 
 int main() {
   // read the rom, start cpu and start decoding
-  FILE *f = fopen("./cpu_instrs/individual/03-op sp,hl.gb", "r");
+
+  log_set_quiet(true);
+
+  FILE *f = fopen("./cpu_instrs/individual/11-op a,(hl).gb", "r");
 
   if (f == NULL) {
     perror("error opening rom for reading");
